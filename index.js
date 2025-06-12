04505edf9fcbf651eed796a2df936796dfaccd55
zakarie <zakarie.abdi@student.moringaschool.com>
2025-06-12 19:18:40 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function countDown(start){
+    while (start >= 0 ){
+        console.log(start);
+        start--;
+    }
+}
+countDown(10);
+
+function writeCards(names, event) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
